be97c1cbb35954ba4442f1964c9b6c196eaf2911
Rene-Ali <renee.guenueruen@gmail.com>
2023-01-05 17:34:48 +0100

    update hypotenuse

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,17 @@ public class Main {
         double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
         JOptionPane.showMessageDialog(null, "Your height is " + height);
 
+        JOptionPane.showMessageDialog(null, "Now we are calculating the hypotenuse with your figures");
+
+        double x = Double.parseDouble(JOptionPane.showInputDialog("Enter X"));
+        double y = Double.parseDouble(JOptionPane.showInputDialog("Enter Y"));
+
+        double z = Math.sqrt((x*x)+(y*y));
+
+        JOptionPane.showMessageDialog(null, "The hypotenuse is " + z);
+
+
+
 
     }
 }
